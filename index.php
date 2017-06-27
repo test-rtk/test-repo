70ff36fbd47fb71b48e77d6d4957b54e05deb431
mdominovic <mdominovic1@gmail.com>
2017-06-27 02:30:31 +0200

    TODO google map

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
             <div class="hero-text-box">
                 <h1>Sugary paradise.</h1>
                 <a class="btn btn-full" href="menu.html">Menu</a>
-                <a class="btn btn-ghost js--scroll-to-start" href="#">View more</a>
+                <a class="btn btn-full js--scroll-to-start" href="#">Where are we?</a>
                 
             </div>
             
@@ -267,15 +267,12 @@
                         <li><a href="#">About us</a></li>
                         <li><a href="#">Blog</a></li>
                         <li><a href="#">Press</a></li>
-                        <li><a href="#">iOS APP</a></li>
                     </ul>
                 </div>
                 <div class="col span-1-of-2">
                     <ul class="social-links">
-                        <li><a href="#"><i class="ion-social-facebook"></i></a></li>
-                        <li><a href="#"><i class="ion-social-twitter"></i></a></li>
-                        <li><a href="#"><i class="ion-social-googleplus"></i></a></li>
-                        <li><a href="#"><i class="ion-social-instagram"></i></a></li>
+                        <li><a href="https://www.facebook.com/mr.cink0s/"><i class="ion-social-facebook"></i></a></li>
+                        <li><a href="https://www.instagram.com/mr.cink0s/"><i class="ion-social-instagram"></i></a></li>
                     </ul>
                 </div>
             </div>
